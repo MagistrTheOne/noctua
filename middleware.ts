7edb87d1674819e503defcfeacc94e0abd63b7c8
MagistrTheOne <maxonyushko71@gmail.com>
2025-10-27 03:33:40 +0300

    Remove internationalization middleware as part of codebase cleanup

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import { middlewareConfig } from './i18n';
-
-export default createMiddleware(middlewareConfig);
-
-export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(ru|en)/:path*']
-};
